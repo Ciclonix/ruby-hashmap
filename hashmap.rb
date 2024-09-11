d7bf31cb8582a1e589f1814d6546cdb138ae8b44
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-11 12:11:27 +0200

    Update hashmap.rb, edit method set to grow the buckets to reduce the number of collisions

--- hashmap.rb
+++ hashmap.rb
@@ -1,13 +1,14 @@
 require_relative "linked_lists"
 
 class HashMap
-  attr_reader :capacity
+  attr_reader :capacity, :length
 
-  INITIAL_CAPACITY = 3
+  INITIAL_CAPACITY = 16
   LOAD_FACTOR = 0.75
 
   def initialize
     @buckets = Array.new(INITIAL_CAPACITY) { LinkedList.new }
+    @length = 0
   end
 
   def hash(key)
@@ -18,17 +19,28 @@ class HashMap
   end
 
   def hashIndex(key)
-    index = hash(key) % length
-    raise ArgumentError if index.negative? || index >= length
+    index = hash(key) % @buckets.length
+    raise ArgumentError if index.negative? || index >= @buckets.length
     return index
   end
 
   def set(key, value)
-    @buckets[hashIndex(key)].append(key, value)
+    if has?(key)
+      @buckets[hashIndex(key)].find(key).value = value
+    else
+      @buckets[hashIndex(key)].append(key, value)
+      @length += 1
+      if length > @buckets.length * LOAD_FACTOR
+        all_entries = entries
+        @buckets = Array.new(@buckets.length * 2) { LinkedList.new }
+        @length = 0
+        all_entries.each { |key, value| set(key, value)}
+      end
+    end
   end
 
   def get(key)
-    return @buckets[hashIndex(key)].find(key, true)
+    return @buckets[hashIndex(key)].find(key).value
   end
 
   def has?(key)
@@ -38,22 +50,21 @@ class HashMap
   def remove(key)
     list = @buckets[hashIndex(key)]
     return nil if list.nil?
-    list.remove_at(list.find(key))
+    list.remove_at(list.find(key, false))
+    @length -= 1
     return list
-  end
-
-  def length
-    return @buckets.length
   end
 
   def clear
     @buckets = Array.new(INITIAL_CAPACITY) { LinkedList.new }
+    @length = 0
   end
 
   def items(to_get) # 0 = keys, 1 = values, 2 = both
     items = []
     @buckets.each do |list|
-      items << list.getItems(to_get).flatten
+      item = list.getItems(to_get).flatten
+      items << item unless item == []
     end
     return items
   end
@@ -67,6 +78,6 @@ class HashMap
   end
 
   def entries
-    return items(2)[0...-1]
+    return items(2)
   end
 end
